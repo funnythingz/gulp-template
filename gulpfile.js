bdd90b0de74db49881cbf85baca3185a404d2a64
funnythingz <funnythingz69+github@gmail.com>
2015-01-15 12:05:41 +0900

    Fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
 
-gulp.task('default', function() {
+gulp.task('jsx', function() {
   return gulp.src('src/jsx/**/*.jsx')
                                .pipe(react())
                                .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  return gulp.watch('src/jsx/**/*.jsx', function(event) {
+                                  gulp.run('jsx');
+                                 });
+});
+
+gulp.task('default', function() {
+  gulp.run('watch');
 });
