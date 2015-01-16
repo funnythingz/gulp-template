596f86050b3166dc5e9628604293e9bcc13c66c2
funnythingz <funnythingz69+github@gmail.com>
2015-01-16 14:11:42 +0900

    modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,135 @@
+// TODO: React
+//var gulp = require('gulp');
+//var react = require('gulp-react');
+//
+//gulp.task('jsx', function() {
+//  return gulp.src('src/jsx/**/*.jsx')
+//                               .pipe(react())
+//                               .pipe(gulp.dest('dist'));
+//});
+//
+//gulp.task('watch', function() {
+//  return gulp.watch('src/jsx/**/*.jsx', function(event) {
+//                                  gulp.run('jsx');
+//                                 });
+//});
+//
+//gulp.task('default', function() {
+//  gulp.run('watch');
+//});
+
 var gulp = require('gulp');
-var react = require('gulp-react');
+var typescript = require('gulp-typescript');
+var compass = require('gulp-compass');
+var concat = require('gulp-concat');
+var slim = require('gulp-slim');
+var uglify = require('gulp-uglify');
+var webserver = require('gulp-webserver');
+var rimraf = require('rimraf');
+var runSequence = require('run-sequence');
+var merge = require('event-stream').merge;
+var plumber = require('gulp-plumber');
 
-gulp.task('jsx', function() {
-  return gulp.src('src/jsx/**/*.jsx')
-                               .pipe(react())
-                               .pipe(gulp.dest('dist'));
+var tsProject = typescript.createProject({
+  target: 'ES5',
+  removeComments: true,
+  sortOutput: true
 });
 
-gulp.task('watch', function() {
-  return gulp.watch('src/jsx/**/*.jsx', function(event) {
-                                  gulp.run('jsx');
-                                 });
+gulp.task('clean', function(cb) {
+  rimraf('build', cb);
 });
 
-gulp.task('default', function() {
-  gulp.run('watch');
+gulp.task('typescript', function() {
+  return gulp.src(['src/ts/*.ts'])
+                               .pipe(plumber())
+                               .pipe(typescript(tsProject, {referencedFrom: ['app.ts']}))
+                               .js
+                               .pipe(concat("app.js"))
+                               .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('compass', function() {
+  return gulp.src('src/scss/*.scss')
+                         .pipe(plumber())
+                         .pipe(compass({
+                          config_file: './config.rb',
+                          comments: false,
+                          css: 'dist/css/',
+                          sass: 'src/scss/',
+                          image: 'dist/imgs/'
+                         }))
+                         .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('slim', function() {
+  return gulp.src('src/slim/*.slim')
+                         .pipe(plumber())
+                         .pipe(slim({
+                          pretty: true
+                         }))
+                         .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('compress', function() {
+  return gulp.src('dist/js/app.js')
+                         .pipe(uglify())
+                         .pipe(concat("app.min.js"))
+                         .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('copy', function() {
+  return merge(
+    gulp.src([
+      './lib/underscore/underscore-min.js',
+      './lib/jquery/dist/jquery.min.js',
+      './lib/jquery.inview/jquery.inview.min.js',
+      './lib/fastclick/lib/fastclick.js',
+      './vendor/js/*.js'
+    ]).pipe(gulp.dest('dist/js/')),
+
+    gulp.src([
+      './vendor/css/*.css'
+    ]).pipe(gulp.dest('dist/css/'))
+  );
+});
+
+gulp.task('assets', function() {
+  return gulp.src([
+    'assets/imgs/*.png',
+    'assets/imgs/*.jpg',
+    'assets/imgs/*.svg'
+  ]).pipe(gulp.dest('dist/imgs/'))
+});
+
+gulp.task('webserver', function() {
+  return gulp.src('dist')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('watch', ['build'], function() {
+  gulp.watch(['src/slim/*.slim', 'src/ts/*.ts', 'src/scss/*.scss', 'assets/*.*'], ['build', 'assets']);
+});
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'typescript',
+    'compress',
+    'slim',
+    'compass',
+    callback
+  );
+});
+
+gulp.task('default', function(callback) {
+  runSequence(
+    'clean',
+    ['copy', 'assets'],
+    'watch',
+    'webserver',
+    callback
+  );
 });
